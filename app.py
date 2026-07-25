78ba0526c4e1c53b6e5fa65e282f6186c74e25ce
Raja Kantu <rajakantu44@gmail.com>
2026-07-25 11:14:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def home():
                 
     return render_template("index.html",output=output)
 if __name__=="__main__":
-    app.run(debug=True)    
+    app.run(host="0.0.0.0",port=5000)    
             
         
